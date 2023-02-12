78e594bddf46114f5b9e4af9286e586d7b30a901
emrht <emrhtfn@gmail.com>
2023-02-13 01:33:24 +0300

    master and worker init scripts updated

--- roles/kvm_provision/files/kvmhost_scripts/customize-commands_master.sh
+++ roles/kvm_provision/files/kvmhost_scripts/customize-commands_master.sh
@@ -4,38 +4,40 @@
 /usr/bin/ssh-keygen -A && \ 
 /usr/bin/apt install ssh -y && \
 /usr/bin/systemctl enable --now ssh && \
-/usr/bin/apt -y install cloud-guest-utils gdisk && \
-/usr/bin/growpart /dev/vda 1 && \
-/usr/sbin/resize2fs /dev/vda1 && \
-/usr/bin/apt install -y apt-transport-https ca-certificates curl gnupg lsb-release && \
-/usr/bin/mkdir -p /etc/apt/keyrings && \
-/usr/bin/curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && \
+/usr/bin/apt -y install cloud-guest-utils gdisk >> /tmp/init.log && \
+/usr/bin/growpart /dev/vda 1 >> /tmp/init.log && \
+/usr/sbin/resize2fs /dev/vda1 >> /tmp/init.log && \
+/usr/bin/apt install -y apt-transport-https ca-certificates curl gnupg lsb-release  >> /tmp/init.log && \
+/usr/bin/mkdir -p /etc/apt/keyrings  >> /tmp/init.log && \
+/usr/bin/curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg  >> /tmp/init.log && \
 /usr/bin/echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-              $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+              $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null  >> /tmp/init.log && \
 /usr/bin/apt update && \
-/usr/bin/apt install containerd.io && \
-/usr/bin/echo overlay >> /etc/modules-load.d/k8s.conf && \
-/usr/bin/echo br_netfilter >> /etc/modules-load.d/k8s.conf && \
-/usr/sbin/modprobe overlay && \
-/usr/sbin/modprobe br_netfilter && \
+/usr/bin/apt install containerd.io  >> /tmp/init.log && \
+/usr/bin/echo overlay >> /etc/modules-load.d/k8s.conf  >> /tmp/init.log && \
+/usr/bin/echo br_netfilter >> /etc/modules-load.d/k8s.conf  >> /tmp/init.log && \
+/usr/sbin/modprobe overlay  >> /tmp/init.log && \
+/usr/sbin/modprobe br_netfilter  >> /tmp/init.log && \
 /usr/bin/echo net.bridge.bridge-nf-call-iptables  = 1 >> /etc/sysctl.d/k8s.conf && \
 /usr/bin/echo net.bridge.bridge-nf-call-ip6tables = 1 >> /etc/sysctl.d/k8s.conf && \
 /usr/bin/echo net.ipv4.ip_forward                 = 1 >> /etc/sysctl.d/k8s.conf && \
-/usr/sbin/sysctl --system && \
-/usr/bin/sed -i 's/cri//' /etc/containerd/config.toml && \
+/usr/sbin/sysctl --system  >> /tmp/init.log && \
+/usr/bin/sed -i 's/cri//' /etc/containerd/config.toml  >> /tmp/init.log && \
 /usr/bin/systemctl restart containerd && \
 /usr/bin/systemctl enable --now containerd && \
 /usr/sbin/swapoff -a && \
 /usr/bin/sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab && \
 /usr/bin/apt update && \
-/usr/bin/apt install -y apt-transport-https ca-certificates curl && \
-/usr/bin/curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg && \
-/usr/bin/echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
+/usr/bin/apt install -y apt-transport-https ca-certificates curl  >> /tmp/init.log && \
+/usr/bin/curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg  >> /tmp/init.log && \
+/usr/bin/echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list  >> /tmp/init.log && \
 /usr/bin/apt update && \
-/usr/bin/apt install -y kubelet kubeadm kubectl && \
-/usr/bin/apt-mark hold kubelet kubeadm kubectl && \
-/usr/bin/kubeadm init --apiserver-advertise-address=192.168.122.20 --apiserver-cert-extra-sans=192.168.122.20 \ 
---pod-network-cidr=10.244.0.0/16 --control-plane-endpoint=192.168.122.10 --node-name master && \
-/usr/bin/kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')" 
+/usr/bin/apt install -y kubelet kubeadm kubectl  >> /tmp/init.log && \
+/usr/bin/apt-mark hold kubelet kubeadm kubectl  >> /tmp/init.log && \
+/usr/bin/kubeadm init --apiserver-advertise-address=192.168.122.20 --apiserver-cert-extra-sans=192.168.122.20 --pod-network-cidr=10.244.0.0/16 --control-plane-endpoint=192.168.122.10 --node-name master  >> /tmp/init.log && \
+export KUBECONFIG=/etc/kubernetes/admin.conf && \
+/usr/bin/echo "export KUBECONFIG=/etc/kubernetes/admin.conf" >> /etc/profile && \
+/usr/bin/kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml >> /tmp/init.log && \
+/usr/bin/python3 -m http.server --directory /tmp
 
 exit 0
--- roles/kvm_provision/files/kvmhost_scripts/customize-commands_worker.sh
+++ roles/kvm_provision/files/kvmhost_scripts/customize-commands_worker.sh
@@ -4,35 +4,39 @@
 /usr/bin/ssh-keygen -A && \ 
 /usr/bin/apt install ssh -y && \
 /usr/bin/systemctl enable --now ssh && \
-/usr/bin/apt -y install cloud-guest-utils gdisk && \
+/usr/bin/apt -y install cloud-guest-utils gdisk  >> /tmp/init.log && \
 /usr/bin/growpart /dev/vda 1 && \
 /usr/sbin/resize2fs /dev/vda1
-/usr/bin/apt install -y apt-transport-https ca-certificates curl gnupg lsb-release && \
+/usr/bin/apt install -y apt-transport-https ca-certificates curl gnupg lsb-release  >> /tmp/init.log && \
 /usr/bin/mkdir -p /etc/apt/keyrings && \
 /usr/bin/curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && \
 /usr/bin/echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
               $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
 /usr/bin/apt update && \
-/usr/bin/apt install containerd.io && \
+/usr/bin/apt install containerd.io  >> /tmp/init.log && \
 /usr/bin/echo overlay >> /etc/modules-load.d/k8s.conf && \
 /usr/bin/echo br_netfilter >> /etc/modules-load.d/k8s.conf && \
-/usr/sbin/modprobe overlay && \
-/usr/sbin/modprobe br_netfilter && \
+/usr/sbin/modprobe overlay &&  >> /tmp/init.log \
+/usr/sbin/modprobe br_netfilter &&  >> /tmp/init.log \
 /usr/bin/echo net.bridge.bridge-nf-call-iptables  = 1 >> /etc/sysctl.d/k8s.conf && \
 /usr/bin/echo net.bridge.bridge-nf-call-ip6tables = 1 >> /etc/sysctl.d/k8s.conf && \
 /usr/bin/echo net.ipv4.ip_forward                 = 1 >> /etc/sysctl.d/k8s.conf && \
-/usr/sbin/sysctl --system && \
-/usr/bin/sed -i 's/cri//' /etc/containerd/config.toml && \
+/usr/sbin/sysctl --system &&  >> /tmp/init.log \
+/usr/bin/sed -i 's/cri//' /etc/containerd/config.toml  >> /tmp/init.log && \
 /usr/bin/systemctl restart containerd && \
 /usr/bin/systemctl enable --now containerd && \
 /usr/sbin/swapoff -a && \
 /usr/bin/sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab && \
 /usr/bin/apt update && \
-/usr/bin/apt install -y apt-transport-https ca-certificates curl && \
+/usr/bin/apt install -y apt-transport-https ca-certificates curl  >> /tmp/init.log && \
 /usr/bin/curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg && \
 /usr/bin/echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
 /usr/bin/apt update && \
-/usr/bin/apt install -y kubelet kubeadm kubectl && \
-/usr/bin/apt-mark hold kubelet kubeadm kubectl
+/usr/bin/apt install -y kubelet kubeadm kubectl  >> /tmp/init.log && \
+/usr/bin/apt-mark hold kubelet kubeadm kubectl  >> /tmp/init.log && \
+/usr/bin/sleep 120 && \
+CERT_HASH=`/usr/bin/curl http://192.168.122.20:8000/init.log 2>&1 | grep -A3 "worker nodes" | grep discovery-token | awk {'print $2'}` && \
+JOIN_TOKEN=`/usr/bin/curl http://192.168.122.20:8000/init.log 2>&1 | grep -A2 "worker nodes" | grep token | awk {'print $5'}` && \
+/usr/bin/kubeadm join 192.168.122.10:6443 --token $JOIN_TOKEN --discovery-token-ca-cert-hash $CERT_HASH >> /tmp/init.log 
 
 exit 0
